d8eed44a111fbab6cf1fadba48e16bda14ce9bb4
Alferguson <alferguson@berkeley.edu>
2017-11-26 13:22:07 -0800

    slight changes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,5 @@
 var path = require("path");
-
+var burger = require("../models/burgerCode.js");
 // Routes
 // =============================================================
 module.exports = function(app) {
