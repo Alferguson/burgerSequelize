bdb677b6f701bfc7912a80311a119f20c1519666
Alferguson <alferguson@berkeley.edu>
2017-11-27 14:16:52 -0800

    its getting there

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var methodOverride = require('method-override')
 
 // Sets up the Express App
 // =============================================================
@@ -27,8 +28,8 @@ app.set("view engine", "handlebars");
 var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
-app.use("/update", routes);
 app.use("/create", routes);
+app.use("/update", routes);
 
 db.sequelize.sync({ force: true }).then(function() {
 	app.listen(PORT, function() {
