a2580119cbcd9f99753c3630925aee69a439510f
Alferguson <alferguson@berkeley.edu>
2017-12-18 09:24:22 -0800

    yep

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var methodOverride = require('method-override')
+var methodOverride = require('method-override');
 
 // Sets up the Express App
 // =============================================================
